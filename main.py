ed80a11c0b744a837f7b5a80e4edc29f91895990
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2019-07-15 07:46:36 +0900

    引数で処理を分けるように実装&jsonで出力するように実装

--- main.py
+++ main.py
@@ -1,43 +1,83 @@
+import sys
 import csv
+import json
 # import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# matplotlibで出力
+def show_sum(date_list, sum_list):
+    # 変換する
+    sum_ary = np.array(sum_list)
+    date_ary = np.array(date_list)
+    # プロットする
+    plt.plot(date_ary, sum_ary)
+    # 出力する
+    plt.show()
+
+# JSONで出力
+def print_json(dict):
+    print(json.dumps(dict, sort_keys=True, indent=4))
+
+
+## main実行
+
+# 引数リスト
+# help, sum, json
+
+args = sys.argv
+flg = 0
+# print(len(args)) args[0]には実行ファイル名が入るみたい
+if len(args) == 1:
+    print("引数を設定してください")
+    print("[sum, json]")
+    exit(1)
+
+# 引数判定(help, sum, json)
+# 第二引数以降は無視する
+if "help" == args[1]:
+    print("help")
+    print("引数は[sum, json]いずれかで指定してください")
+    exit(0)
+elif "sum" == args[1]:
+    print("sum")
+    flg = 1
+elif "json" == args[1]:
+    print("json")
+    flg = 2
+else:
+    print("引数エラーです")
+    print("引数は[sum, json]いずれかで指定してください")
+    print("困った場合はhelpをください")
+    exit(0)
+
 # 対象のファイルパスを選択
-file_path = 'files/data.csv'
+FILE_PATH = 'files/data.csv'
 
 ## Pandasを使用して読み込む場合
-# df = pd.read_csv(file_path)
+# df = pd.read_csv(FILE_PATH)
 # print(df)
 # print(df['date'])
 
-## 普通にCSVを読み込む場合
-
-with open(file_path, 'r') as f:
+## 普通にCSVを読み込む場合(withを使うと安全に読み込める)
+with open(FILE_PATH, 'r') as f:
     # ファイルを読み込む
     reader = csv.reader(f)
     # ヘッダ行を除く
     header = next(reader)
-
-    # print(header)
-    # リスト型に辞書型を格納する
+    # 変数宣言
     sum_list = []
     date_list = []
+    json_dict = {}
     for row  in reader:
-        # sum_dict = {}
         # 合計を算出する
         sum = int(row[1]) + int(row[2]) + int(row[3]) + int(row[4])
-        # sum_dict[row[0]] = sum
-        # sum_list.append(sum_dict)
         sum_list.append(sum)
         date_list.append(int(row[0]))
+        json_dict[row[0]] = sum
 
-        
-# 辞書型に想定どおりに入っていることを確認
-# print(sum_list)
-
-sum_ary = np.array(sum_list)
-date_ary = np.array(date_list)
-
-plt.plot(date_ary, sum_ary)
-plt.show()
+if flg == 1:
+    show_sum(date_list, sum_list)
+elif flg == 2:
+    print_json(json_dict)
