632751fe41d684a7acecf1e5f9b7f9e480ca8d60
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2019-07-14 22:28:25 +0900

    CSVのファイル読み込みを追加

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("a")
+import csv
+import pandas as pd
+
+file_path = 'files/data.csv'
+
+## 普通にCSVを読み込む場合
+
+# with open(file_path, 'r') as f:
+#     reader = csv.reader(f)
+#     header = next(reader)
+
+#     print(header)
+
+#     for row  in reader:
+#         print(row)
+
+
+## Pandasを使用して読み込む場合(こっちが良い)
+df = pd.read_csv(file_path)
+print(df)
+print(df['date'])
